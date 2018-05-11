41d508080f9ad4c75aed73f4b0c8786020941401
Travis CI User <travis@example.org>
2018-05-11 16:57:10 +0000

    Update docs

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to EthPM Package Manifest Specification\u2019s documentation!"],titleterms:{document:0,ethpm:0,indic:0,manifest:0,packag:0,specif:0,tabl:0,welcom:0}})
