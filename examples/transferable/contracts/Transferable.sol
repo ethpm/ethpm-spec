481739f6138907db88602558711e9d3c1301c269
Nick Gheorghita <nickgheorghita@gmail.com>
2018-05-25 12:31:30 -0600

    Update solidity and ipfs uris for transferable pkg

---
+++ examples/transferable/contracts/Transferable.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.24;
+
+import {Owned} from "owned/contracts/Owned.sol";
+
+contract Transferable is Owned {
+    event OwnerChanged(address indexed prevOwner, address indexed newOwner);
+
+    function transferOwner(address newOwner) public onlyOwner returns (bool) {
+        emit OwnerChanged(owner, newOwner);
+        owner = newOwner;
+        return true;
+    }
+}
