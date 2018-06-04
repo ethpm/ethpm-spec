7730cdc61f29ed27f7cceeb061048be19b3e7e05
Nick Gheorghita <nickgheorghita@gmail.com>
2018-06-04 17:05:41 -0600

    Update escrow package resources

--- examples/escrow/contracts/SafeSendLib.sol
+++ examples/escrow/contracts/SafeSendLib.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.24;
 
 
 /// @title Library for safe sending of ether.
@@ -7,10 +7,12 @@ library SafeSendLib {
     /// @dev Attempts to send the specified amount to the recipient throwing an error if it fails
     /// @param recipient The address that the funds should be to.
     /// @param value The amount in wei that should be sent.
-    function sendOrThrow(address recipient, uint value) returns (bool) {
-        if (value > this.balance) throw;
+    function sendOrThrow(address recipient, uint value) public returns (bool) {
+        if (value > address(this).balance)
+            revert();
 
-        if (!recipient.send(value)) throw;
+        if (!recipient.send(value))
+            revert();
 
         return true;
     }
