9ad35851f3440fa5dd22ae89c3970205eb320020
Nick Gheorghita <nickgheorghita@gmail.com>
2018-06-04 11:34:31 -0600

    Make solidity functions public

--- examples/safe-math-lib/contracts/SafeMathLib.sol
+++ examples/safe-math-lib/contracts/SafeMathLib.sol
@@ -7,7 +7,7 @@ library SafeMathLib {
     /// @dev Adds a and b, throwing an error if the operation would cause an overflow.
     /// @param a The first number to add
     /// @param b The second number to add
-    function safeAdd(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    function safeAdd(uint256 a, uint256 b) public pure returns (uint256 c) {
         c = a + b;
         assert(c >= a);
         return c;
@@ -16,7 +16,7 @@ library SafeMathLib {
     /// @dev Subtracts b from a, throwing an error if the operation would cause an underflow.
     /// @param a The number to be subtracted from
     /// @param b The amount that should be subtracted
-    function safeSub(uint256 a, uint256 b) internal pure returns (uint256) {
+    function safeSub(uint256 a, uint256 b) public pure returns (uint256) {
         assert(b <= a);
         return a - b;
     }
