6789cefff02886621b51859aca1fbaca034e338a
Nick Gheorghita <nickgheorghita@gmail.com>
2019-05-14 11:00:07 +0200

    Fix sphinx build errors

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,7 +82,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = []
+exclude_patterns = ['_use-cases.rst']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
